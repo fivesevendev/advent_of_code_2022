92b248156c893a15dcda1f2c170248876fc0773e
fivesevendev <github@fiveseven.dev>
2022-12-01 22:52:45 -0500

    Add template

---
+++ template.py
@@ -0,0 +1,18 @@
+#AOC-2022-
+
+
+import timeit
+import time
+
+
+def numFind(n):
+    pass
+
+
+if __name__ == '__main__':
+    startTime = timeit.default_timer()
+    print(">>>>>", time.asctime(), "<<<<<\n")
+    n = 0
+    print("Result:", numFind(n))
+    print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
+    print("\n>>>>>", time.asctime(), "<<<<<")
