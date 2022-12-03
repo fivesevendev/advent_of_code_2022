13ae59828dadf9eb9f394e1018048b447299016f
fivesevendev <github@fiveseven.dev>
2022-12-03 13:49:07 -0500

    Solution 3-1

---
+++ 3-1.py
@@ -0,0 +1,33 @@
+#AOC-2022-3-1
+
+
+import timeit
+import time
+
+
+def numFind(n):
+    output = 0
+    with open("3.txt") as f:
+        f = f.read()
+    f = [r for r in f.split("\n")]
+    for i in f:
+        a = set(i[:len(i) // 2])
+        b = set(i[len(i) // 2:])
+        x = list(a.intersection(b))[0]
+        #print(priority(x))
+        output += priority(x)
+    return output        
+
+def priority(n):
+    if n.islower():
+        return (ord(n) - 96)
+    return ord(n) - 38
+
+
+if __name__ == '__main__':
+    startTime = timeit.default_timer()
+    print(">>>>>", time.asctime(), "<<<<<\n")
+    n = 0
+    print("Result:", numFind(n))
+    print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
+    print("\n>>>>>", time.asctime(), "<<<<<")
