c4920d3c4aaed2c4988238ac2ff62af92a33d486
fivesevendev <github@fiveseven.dev>
2022-12-04 08:55:16 -0500

    Solution 2-2

--- 2-2.py
+++ 2-2.py
@@ -6,8 +6,7 @@ import time
 
 
 key = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
-throw = ['#', 'A', 'B', 'C']
-
+throw = ['C', 'A', 'B']
 
 def numFind(n):
     score = 0
@@ -16,9 +15,9 @@ def numFind(n):
     f = [tuple(x.split(" ")) for x in f.split("\n")]
     for i, j in f:
         if j == "X":
-            score += key[throw[round((throw.index(i) + 2) % 3.001)]]
+            score += key[throw[(throw.index(i) + 2) % 3]]
         elif j == "Z":
-            score += key[throw[round((throw.index(i) + 1) % 3.001)]] + 6
+            score += key[throw[(throw.index(i) + 1) % 3]] + 6
         else:
             score += key[i] + 3
     return score
