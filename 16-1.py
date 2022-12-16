df122f8fa8062a4f07afe8d57a4d8847fa30fcfa
fivesevendev <github@fiveseven.dev>
2022-12-16 12:54:58 -0500

    wip

---
+++ 16-1.py
@@ -0,0 +1,34 @@
+#AOC-2022-16-1
+
+
+import timeit
+import time
+
+
+def numFind():
+    valves = {}
+    with open("16.txt") as f:
+        f = f.read()
+    f = [s for s in f.split("\n")]
+    for ff in range(len(f)):    
+        f[ff] = f[ff].replace("Valve", "")
+        f[ff] = f[ff].replace("has flow rate=", ",")
+        f[ff] = f[ff].replace("els", "el").replace("ads", "ad").replace("ves", "ve")
+        f[ff] = f[ff].replace("; tunnel lead to valve", ",")
+        f[ff] = f[ff].replace(" ", "")
+    
+    for vf in f:
+        v = vf.split(",")
+        valves[v[0]] = {"flow": int(v[1]), "leads": v[2:], "open": False}
+    
+    for v in valves:
+        #print(v)
+        print(valves[v]["flow"])
+
+
+if __name__ == '__main__':
+    startTime = timeit.default_timer()
+    print(">>>>>", time.asctime(), "<<<<<\n")
+    print("Result:", numFind())
+    print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
+    print("\n>>>>>", time.asctime(), "<<<<<")
