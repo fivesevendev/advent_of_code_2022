51838af9fae9c827dffb9cb77ba999536f846226
fivesevendev <github@fiveseven.dev>
2022-12-18 21:06:26 -0500

    wip

--- 15-2.py
+++ 15-2.py
@@ -3,9 +3,9 @@
 
 import timeit
 import time
+import sys
 
-
-grid = {}
+grid = []
 
 
 def numFind(mx):
@@ -21,51 +21,61 @@ def numFind(mx):
         f[line] = list(map(int, f[line].split(",")))
     progress = 1
     for ff in f:
-        print("Line: ", progress)
+        print("Progress:", progress)
+        kill = []
         width = 0
-        sensorCenter = (ff[1], ff[0])
-        beacon = (ff[3], ff[2])
-        grid[sensorCenter] = "S"
-        grid[beacon] = "B"
+        sensorCenter = [ff[1], ff[0]]
+        beacon = [ff[3], ff[2]]
+        grid.append(sensorCenter)
+        grid.append(beacon)
         mD = manhat(sensorCenter, beacon)
+        grid.append([ff[1] - 1, ff[0]]) #sets exclusion one row higher than peak
         while width <= mD:            
             r = ff[1] - (mD - width)
-            for c in range(ff[0] - width, ff[0] + width + 1): #starts top peak of diamond going thru base
-                if c > mx or c < 0 or r > mx or r < 0: #checks if its too big or too small for area of concern
-                    continue
-                grid[(r, c)] = None
+            grid.append([r, ff[0] - width - 1]) #set a point left side of boundary
+            grid.append([r, ff[0] + width + 1]) #set a point right side of boundary
+            for check in grid:
+                if (ff[0] - width - 1) < check[1] < (ff[0] + width + 1):
+                    kill.append(check)
             width += 1
         width -= 2
         while width >= 0:
             r = ff[1] + (mD - width)
-            for c in range(ff[0] - width, ff[0] + width + 1): #starts line under base thru bottom peak
-                if c > mx or c < 0 or r > mx or r < 0: #checks if its too big or too small for area of concern
-                    continue
-                grid[(r, c)] = None
+            grid.append([r, ff[0] - width - 1]) #set a point left side of boundary
+            grid.append([r, ff[0] + width + 1]) #set a point right side of boundary
+            for check in grid:
+                #print("check:", check, "r:", r)
+                if (ff[0] - width - 1) < check[1] < (ff[0] + width + 1):
+                    kill.append(check)
             width -= 1
+        grid.append([ff[1] + 1, ff[0]]) #sets exclusion one row lower than peak
+        #print("old:", sorted(grid))
+        #print()
+        #print("kill:", sorted(kill))
+        #print()
+        for k in kill:
+            if k in grid:
+                grid.remove(k)
+        #print("new", sorted(grid))
+        #break
+        #sys.exit()
         progress += 1
-    #print(sorted([g for g in grid if 0 <= g[0] <= mx and 0 <= g[1] <= mx]))
-    print("Starting To Look For Missing")
-    #return missing(mx)
-    #return "*****WIP*****"
-
+    for q in grid:
+        if 0 <= q[0] <= mx and 0 <= q[1] <= mx:
+            print(q, end=", ")
+    print()
+    print("len:", len(grid))
 
 def manhat(a, b):
     aa = abs(a[0] - b[0])
     bb = abs(a[1] - b[1])
     return aa + bb
-
-def missing(mx):
-    for row in range(mx):
-        for column in range(mx):
-            if (row, column) not in grid:
-                return (column * 4000000) + row
     
 
 if __name__ == '__main__':
     startTime = timeit.default_timer()
     print(">>>>>", time.asctime(), "<<<<<\n")
-    mx = 4000000
+    mx = 20
     print("Result:", numFind(mx))
     print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
     print("\n>>>>>", time.asctime(), "<<<<<")
--- 16-1.py
+++ 16-1.py
@@ -3,10 +3,14 @@
 
 import timeit
 import time
+from copy import deepcopy
 
 
 def numFind():
-    valves = {}
+    valve_info = {}
+    valve_state = {}
+    #paths = [['AA']]
+    paths = [['AA', 'DD', 'CC'], ['AA', 'DD', 'AA'], ['AA', 'DD', 'EE'], ['AA', 'II', 'AA'], ['AA', 'II', 'JJ'], ['AA', 'BB', 'CC'], ['AA', 'BB', 'AA']]
     seconds = 30
     with open("16.txt") as f:
         f = f.read()
@@ -17,20 +21,43 @@ def numFind():
         f[ff] = f[ff].replace("els", "el").replace("ads", "ad").replace("ves", "ve")
         f[ff] = f[ff].replace("; tunnel lead to valve", ",")
         f[ff] = f[ff].replace(" ", "")
-    
     for vf in f:
         v = vf.split(",")
-        valves[v[0]] = {"flow": int(v[1]), "leads": v[2:], "open": False} #builds the valves dict with attributes
-    
-    while seconds > 0:
-        break
+        valve_info[v[0]] = {"flow": int(v[1]), "leads": v[2:]} #builds the valve_info dict with attributes
+        valve_state[v[0]] = False #builds the valve_state dict with state
 
-    return "*****WIP*****"
+    #for v, vi, vs in zip(valve_state, valve_info.values(), valve_state.values()):
+    #    print(v, vi, vs)
     
+    #print(valve_info['AA'], valve_state['AA'])
+    steps = 2
+
+    while steps < 30:
+        new_paths = []
+        for path in paths:
+            if len(set(path)) == 2:
+                continue
+            else:
+                cur_pos = path[-1]
+                for next_step in valve_info[cur_pos]["leads"]:
+                    new_paths.append(path + [next_step])
+        paths = deepcopy(new_paths)
+        print("Step:", steps, "Num of Paths:", len(paths))
+        steps += 1
+    #print("All Paths:", paths)
+    print("path len:", len(paths[5]))
+    return list(valve_state.values()).count(False) #use this in the while loop to detect if all valves are open
+    
+
+
+
+
+
 
 if __name__ == '__main__':
     startTime = timeit.default_timer()
     print(">>>>>", time.asctime(), "<<<<<\n")
     print("Result:", numFind())
+    print('*******WIP*********')
     print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))
     print("\n>>>>>", time.asctime(), "<<<<<")
